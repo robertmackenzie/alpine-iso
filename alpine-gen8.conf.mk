a5e4624da5813bcc08db36c4ca393eae02563baf
Robert Mackenzie <robertmackenzie@gmail.com>
2017-05-14 16:57:33 +0100

    Build for Gen8 Microserver
    Should be able to use SOL via the iLO from BIOS, to bootloader, to
    getty.

---
+++ alpine-gen8.conf.mk
@@ -0,0 +1,5 @@
+ALPINE_NAME     := alpine-gen8
+KERNEL_FLAVOR   := grsec
+MODLOOP_EXTRA	= $(addsuffix -grsec, dahdi-linux xtables-addons)
+SYSLINUX_SERIAL	:= serial 0 115200
+BOOT_OPTS	:= console=tty0 console=ttyS0,115200
