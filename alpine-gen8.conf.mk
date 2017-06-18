425eb7c7b0e4b148e0d5d176bdf523fd813ccc9a
Robert Mackenzie <robertmackenzie@gmail.com>
2017-06-18 20:22:34 +0000

    Add ZFS to Gen8

--- alpine-gen8.conf.mk
+++ alpine-gen8.conf.mk
@@ -1,5 +1,5 @@
 ALPINE_NAME     := alpine-gen8
 KERNEL_FLAVOR   := grsec
-MODLOOP_EXTRA	= $(addsuffix -grsec, dahdi-linux xtables-addons)
+MODLOOP_EXTRA	= $(addsuffix -grsec, dahdi-linux xtables-addons spl zfs)
 SYSLINUX_SERIAL	:= serial 0 115200
 BOOT_OPTS	:= console=tty0 console=ttyS0,115200
